f04087b2638d0cb52c8cfd2bbd3e17d4560d017d
sbean585 <sbean585@gmail.com>
2023-01-31 18:00:05 -0500

    Update Tutorial_01_3.m

--- Ch01/Tutorial_01_3.m
+++ Ch01/Tutorial_01_3.m
@@ -15,3 +15,31 @@ fprintf('Output for Tutorial_01_2 written by John Wadach.\n\n')
 
 % Variable definitions
 RowVector=[1 2 3 4 5 ] % row vector variable
+MatrixA=[1 2 3 4;5 6 7 8;9 10 11 12] 5 matrix variable
+
+% The old way of finding the average value of a row vector.
+SumRow= 0 % initialize the sum to 0
+for  col=1:1:5 % repeats the loop starting with col=1, increments col by 1,
+                %ends with col=5
+      SumRow=SumRow + RowVector(col) % adds the current vector valule to sum
+end
+OldRowAverage=SumRow/5 % computes the average
+
+% The modern way of finding a row average
+SizeRowVector=size(RowVector) %The size() vecotr return the number of
+                                  % row and number of columns.
+                                  % SizeRowVector(1) = number of rows = 1
+                                  % SizeRowVector(2)= number of columns= 5
+% The sum() function computes the sum of all elements of RowVector
+ModernRowAverage=sum(RowVector)/SizeRowVector(2)
+
+% The old way of squaring each element of amatrix
+for col=1:1:4 %accesses each column in MatrixA
+    for row=1:1:3 % accesses each row in MatrixA
+        MatrixOld(row,col)=MatrixA(row,col)^2; % squares each element
+    end
+end
+
+%  output the MatrixA
+MatrixOld
+
